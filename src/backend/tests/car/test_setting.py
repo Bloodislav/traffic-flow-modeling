d99f7c4c837e198f136ccf427a2527973904d4f0
Ronin Shawanda <roninsha@student.21-school.ru>
2024-06-16 16:36:13 +0500

    Styling

--- src/backend/tests/car/test_setting.py
+++ src/backend/tests/car/test_setting.py
@@ -4,12 +4,7 @@ import pytest
 
 @pytest.fixture
 def car_object() -> Car:
-    return Car(
-        x=0, 
-        y=0, 
-        max_speed=17, 
-        koeff=1.01
-    )
+    return Car(x=0, y=0, max_speed=17, koeff=1.01)
 
 
 @pytest.fixture
--- src/backend/tests/car/test_update.py
+++ src/backend/tests/car/test_update.py
@@ -12,14 +12,17 @@ def test_update_x_1(car_object: Car, a_accel: int):
     assert car_object.x == a_accel
     assert car_object.speed_x > 0
 
+
 def test_update_x_2(car_object: Car, a_accel: int):
     car_object.accelerate_x(a_accel)
-    
-    for _ in range(5): car_object.update()
+
+    for _ in range(5):
+        car_object.update()
 
     assert car_object.y == 0
     assert car_object.x > 0
     assert car_object.speed_x == 0
+
 
 def test_update_x_3(car_object: Car, a_accel: int):
     n = 20
@@ -39,13 +42,14 @@ def test_update_compex_1(car_object: Car, a_accel: int):
     car_object.update()
     assert car_object.x == a_accel
     assert car_object.y == a_accel
-    
+
     assert car_object.speed_x == a_accel - car_object.speed_stop
     assert car_object.speed_y == a_accel - car_object.speed_stop
-    
+
+
 def test_update_compex_2(car_object: Car, a_accel: int):
     n = 20
-    
+
     for _ in range(n):
         car_object.accelerate_x(a_accel)
         car_object.accelerate_y(a_accel)
@@ -54,4 +58,4 @@ def test_update_compex_2(car_object: Car, a_accel: int):
     assert car_object.x == 243
     assert car_object.y == 108
     assert car_object.speed_x > 20
-    assert car_object.speed_y >= 6  
+    assert car_object.speed_y >= 6
