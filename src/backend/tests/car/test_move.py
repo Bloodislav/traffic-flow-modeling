ba29ad432009146887d26becd7d754719ec59cdf
Ronin Shawanda <roninsha@student.21-school.ru>
2024-06-16 16:31:44 +0500

    Add tests for Car's methods

---
+++ src/backend/tests/car/test_move.py
@@ -0,0 +1,36 @@
+from models.car import Car
+from test_setting import car_object, a_accel
+
+
+# ----=== Speed ===--- #
+def test_move_1(car_object: Car, a_accel: int):
+    car_object.accelerate_x(a_accel)
+    car_object.move()
+
+    assert car_object.speed_x == a_accel
+
+
+def test_move_2(car_object: Car, a_accel: int):
+    car_object.accelerate_y(a_accel)
+    car_object.move()
+
+    assert car_object.speed_y == a_accel
+
+
+def test_move_3(car_object: Car, a_accel: int):
+    car_object.accelerate_y(a_accel)
+    car_object.accelerate_x(a_accel)
+    car_object.move()
+
+    assert car_object.speed_y == a_accel
+    assert car_object.speed_x == a_accel
+
+
+def test_move_4(car_object: Car, a_accel: int):
+    car_object.accelerate_y(-a_accel)
+    car_object.accelerate_x(-a_accel)
+
+    car_object.move()
+
+    assert car_object.speed_y == -a_accel
+    assert car_object.speed_x == -a_accel
---
+++ src/backend/tests/car/test_update.py
@@ -0,0 +1,57 @@
+import pytest
+from models.car import Car
+from test_setting import car_object, a_accel
+
+
+# ----=== Uppdate ===--- #
+def test_update_x_1(car_object: Car, a_accel: int):
+    car_object.accelerate_x(a_accel)
+    car_object.update()
+
+    assert car_object.y == 0
+    assert car_object.x == a_accel
+    assert car_object.speed_x > 0
+
+def test_update_x_2(car_object: Car, a_accel: int):
+    car_object.accelerate_x(a_accel)
+    
+    for _ in range(5): car_object.update()
+
+    assert car_object.y == 0
+    assert car_object.x > 0
+    assert car_object.speed_x == 0
+
+def test_update_x_3(car_object: Car, a_accel: int):
+    n = 20
+    for _ in range(n // 2):
+        car_object.accelerate_x(a_accel)
+        car_object.update()
+    for _ in range(n // 2):
+        car_object.update()
+    assert car_object.x > 0
+    assert car_object.speed_x > 0
+
+
+def test_update_compex_1(car_object: Car, a_accel: int):
+    car_object.accelerate_x(a_accel)
+    car_object.accelerate_y(a_accel)
+
+    car_object.update()
+    assert car_object.x == a_accel
+    assert car_object.y == a_accel
+    
+    assert car_object.speed_x == a_accel - car_object.speed_stop
+    assert car_object.speed_y == a_accel - car_object.speed_stop
+    
+def test_update_compex_2(car_object: Car, a_accel: int):
+    n = 20
+    
+    for _ in range(n):
+        car_object.accelerate_x(a_accel)
+        car_object.accelerate_y(a_accel)
+        car_object.update()
+
+    assert car_object.x == 243
+    assert car_object.y == 108
+    assert car_object.speed_x > 20
+    assert car_object.speed_y >= 6  
