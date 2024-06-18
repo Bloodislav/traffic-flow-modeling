d346a9a2a2953b30802e292dd26bfbc35880e4ba
Ronin Shawanda <roninsha@student.21-school.ru>
2024-06-18 16:40:24 +0500

    Add Trayber model by y coord

--- src/backend/models/car.py
+++ src/backend/models/car.py
@@ -1,15 +1,26 @@
 import math
 from abc import ABC
 
+# TODO DTO
+
 
 class CarInterface(ABC):
     def __init__(self) -> None:
+        self.length: float = 4.0
         self.angle: int = 0
-        self.speed: float = 0
-        self.prev_speed: float = 0
-        self.acceleration: float = 0
-        self.a_accceler: float = 25.0 / 10
-        self.b_accceler: float = 25.0 / 10
+
+        self.a_accceler: float = 2
+        self.b_accceler: float = 2
+
+        self.speed_stop: float = 0.5
+        self.prev_speed_x: float = 0
+        self.prev_speed_y: float = 0
+
+        self.speed_x: float = 0
+        self.speed_y: float = 0
+
+        self.a_x: float = 0
+        self.a_y: float = 0
 
     def accelerate() -> None: ...
 
@@ -20,43 +31,101 @@ class Car(CarInterface):
     def __init__(self, x: int, y: int, max_speed: float, koeff: float) -> None:
         super().__init__()
         """Инициализация машины."""
+        self.a_accceler: float = 2.5
+        self.b_accceler: float = 2.5
+
         self.x: int = x
         self.y: int = y
         self.koeff: float = koeff
-        self.max_speed: float = max_speed
+        self.max_speed_x: float = max_speed
+        self.max_speed_y: float = max_speed / 5
 
-    def accelerate(self, turn: int, acceleration: float) -> None:
-        """Ускорение / замедление автомобиля."""
-        if turn != 0:
-            self.acceleration = (
-                turn
-                * acceleration
-                * (1 - (self.prev_speed / self.max_speed) ** self.koeff)
-            )
-        elif self.speed != 0:
-            self.acceleration = acceleration if self.speed.real < 0 else -acceleration
-
+    # ---=== Another ===--- #
+    # ! Don't test
     def turn(self, angle_change: float) -> None:
         """Поворот машины на заданный угол."""
-        self.angle += int(angle_change * abs(self.speed / self.max_speed))
-
-    def move(self) -> None:
-        """Обмновление скорости"""
-        self.prev_speed = self.speed
-        self.speed += self.acceleration
+        if angle_change >= 0:
+            self.angle = min(self.angle + angle_change, 30)
+        else:
+            self.angle = max(self.angle + angle_change, -30)
 
     def stop(self) -> None:
-        """Остановка машины."""
-        self.speed, self.acceleration = [0, 0]
+        self.stop_x()
+        self.stop_y()
+
+    # ---=== Accelerate | Speed ===--- #
+    def accelerate_func(
+        self, acceleration: float, prev_sped: float, max_speed: float
+    ) -> float:
+        res: float = acceleration * (1 - (prev_sped / max_speed) ** self.koeff)
+        return res.real
+
+    def _deceleration(self, speed: float) -> None:
+        # TODO
+        accel: float = -self.speed_stop if self.speed_x > 0 else self.speed_stop
+
+    # ------ Cord X ------ #
+    def accelerate_x(self, acceleration: float) -> None:
+        self.a_x = self.accelerate_func(
+            acceleration, self.prev_speed_x, self.max_speed_x
+        )
+
+    def stop_x(self) -> None:
+        self.speed_x, self.a_x = [0, 0]
+
+    def _speed_x_limit(self, speed: float, max_speed: float) -> float:
+        result: float = (
+            min(speed.real, max_speed)
+            if speed.real >= 0
+            else max(speed.real, -max_speed / 3)
+        )
+        return result
+
+    def _deceleration_x(self) -> None:
+        accel: float = -self.speed_stop if self.speed_x > 0 else self.speed_stop
+        self.accelerate_x(accel)
+        self.speed_x += self.a_x
+
+    # ------ Cord Y ------ #
+    def accelerate_y(self, acceleration: float) -> None:
+        self.a_y = self.accelerate_func(
+            acceleration, self.prev_speed_y, self.max_speed_y
+        )
+
+    def stop_y(self) -> None:
+        self.speed_y, self.a_y = [0, 0]
+
+    def _speed_y_limit(self, speed: float, max_speed: float) -> float:
+        result: float = (
+            min(speed.real, max_speed)
+            if speed.real >= 0
+            else max(speed.real, -max_speed)
+        )
+        return result
+
+    def _deceleration_y(self) -> None:
+        accel: float = -self.speed_stop if self.speed_y > 0 else self.speed_stop
+        self.accelerate_y(accel)
+        self.speed_y += self.a_y
+
+    # ---=== Move | Update ===--- #
+    def move(self) -> None:
+        """Обмновление скорости"""
+        self.prev_speed_x = self.speed_x
+        self.speed_x += self.a_x
+        self.speed_x = self._speed_x_limit(self.speed_x, self.max_speed_x)
+
+        self.prev_speed_y = self.speed_y
+        self.speed_y += self.a_y
+        self.speed_y = self._speed_y_limit(self.speed_y, self.max_speed_y)
 
     def update(self) -> None:
         """Обновление позиции авто."""
-        # Ограничиваем скорость максимальным значением
-        if self.speed.real >= 0:
-            self.speed = min(self.speed.real, self.max_speed)
-        else:
-            self.speed = max(self.speed.real, -self.max_speed / 3)
+        self.move()
 
-        # Обновляем позицию автомобиля
-        self.x += int(math.cos(math.radians(-self.angle)) * self.speed.real)
-        self.y += int(math.sin(math.radians(-self.angle)) * self.speed.real)
+        self.x += int(self.speed_x.real)
+        self.y += int(self.speed_y.real)
+
+        # Сброс скорости
+        self._deceleration_x() if abs(self.speed_x) > self.speed_stop else self.stop_x()
+        self._deceleration_y() if abs(self.speed_y) > self.speed_stop else self.stop_y()
--- src/backend/models/car_ai.py
+++ src/backend/models/car_ai.py
@@ -1,8 +1,12 @@
 import numpy as np
 from math import pow
 from scipy.interpolate import interp1d
+from scipy.integrate import simps, quad
 
 from .car import Car
+
+# TODO: динамическое определние диставнции
+# TODO DTO
 
 
 class CarAi(Car):
@@ -13,14 +17,14 @@ class CarAi(Car):
         max_speed: float,
         koeff: float,
         lead_car: Car,
-        distance: int,
     ) -> None:
         """Инициализация ии_машины."""
         super().__init__(x, y, max_speed, koeff)
         self.lead_car: Car = lead_car
-        self.distance = distance
-        self.curent_distance = distance
+        self.distance_x: float = 0
+        self.distance_y: float = 10
 
+    # ---=== Another ===--- #
     def ro_func(self, h: float, h_max: float, d: float) -> float:
         """"""
         res: float = 0
@@ -36,7 +40,7 @@ class CarAi(Car):
         """"""
         res: float = 0
         x = np.array([0, 5, 20, 45, 80, 120, 200, 1000])
-        y = np.array([0, 25, 50, 75, 105, 125, 201, 1000])
+        y = np.array([5, 25, 50, 75, 105, 125, 201, 1000])
         f2 = interp1d(x, y, kind="cubic")
 
         if h > h_max:
@@ -45,23 +49,85 @@ class CarAi(Car):
 
         return res.real
 
+    # ---=== Accelerate | Speed ===--- #
     def accelerate(self) -> None:
         """Закон изменения ускорения по модели следования за лидером"""
+        # diff_x: int = self.lead_car.x - self.x
+
+        # ro = self.ro_func(diff_x, self.distance_x, 35)
+        # v_h = self.v_h_func(diff_x, self.distance_x, self.max_speed_x)
+
+        # koef_1 = self.a_accceler * (1 - (self.speed_x / self.max_speed_x) ** self.koeff)
+        # koef_2 = self.b_accceler * (self.speed_x - v_h)
+
+        # if self.lead_car.speed.real >= 0:
+        #     self.a = (ro) * koef_1 + (ro - 1) * koef_2
+        # else:
+        #     self.a = (ro - 1) * koef_1 - (ro) * koef_2
+
+    # ------ Cord X ------
+    def _need_distance(self) -> None:
+        self.distance_x = (
+            self.length
+            + 2.2 * self.prev_speed_x
+            + (1 / self.lead_car.b_accceler - 1 / self.b_accceler)
+            * self.prev_speed_x**2
+            / 2
+            * 1.1
+            # !
+            + 100
+        )
+
+    def target_function_f(self, x) -> float:
+        return self.lead_car.speed_x - self.speed_x
+
+    def accelerate_ai_x(self) -> None:
+        self._need_distance()
+
         diff_x: int = self.lead_car.x - self.x
-        ro = self.ro_func(diff_x, self.distance, 35)
-        v_h = self.v_h_func(diff_x, self.distance, self.max_speed)
+        diff_x_v: float = quad(self.target_function_f, 0, self.max_speed_x)
+        diff_x_v = diff_x_v[0]
 
-        koef_1 = self.a_accceler * (1 - (self.speed / self.max_speed) ** self.koeff)
-        koef_2 = self.b_accceler * (self.speed - v_h)
+        ro = self.ro_func(diff_x, self.distance_x, self.distance_x / 2)
+        v_h = self.v_h_func(diff_x_v, self.distance_x, self.max_speed_x)
 
-        if self.lead_car.speed.real >= 0:
-            self.acceleration = (ro) * koef_1 + (ro - 1) * koef_2
+        koef_1 = self.a_accceler * (
+            1 - (self.prev_speed_x / self.max_speed_x) ** self.koeff
+        )
+        koef_2 = self.b_accceler * (self.speed_x - v_h)
+
+        if self.lead_car.speed_x.real >= 0:
+            self.a_x = (ro) * koef_1 + (ro - 1) * koef_2
         else:
-            self.acceleration = (ro - 1) * koef_1 - (ro) * koef_2
+            self.a_x = (ro - 1) * koef_1 - (ro) * koef_2
 
-    def move(self) -> None:
+        # self.a_x = self.a_x if abs(self.a_x.real * 0.87 ** 2 / self.distance_x) >= self.a_accceler / 15 else 0
+
+    # ------ Cord Y ------ #
+    def accelerate_ai_y(self) -> None:
         diff_y: int = self.lead_car.y - self.y
+        ro = self.ro_func(diff_y, 2, 15)
+        v_h = self.v_h_func(diff_y, 2, self.max_speed_y)
 
-        self.prev_speed = self.speed
-        self.accelerate()
-        self.speed += float(self.acceleration.real)
+        koef_1 = self.a_accceler * (
+            1 - (self.prev_speed_y / self.max_speed_y) ** self.koeff
+        )
+        koef_2 = self.b_accceler * (self.speed_y - v_h)
+
+        if self.lead_car.speed_y.real >= 0:
+            self.a_y = (ro) * koef_1 + (ro - 1) * koef_2
+        else:
+            self.a_y = (ro - 1) * koef_1 - (ro) * koef_2
+
+        self.a_y = self.a_y if abs(self.a_y.real * 0.87**2 / 2) >= 0.3 else 0
+
+    # ---=== Move ===--- #
+    def move(self) -> None:
+        self.prev_speed_x = self.speed_x
+        self.prev_speed_y = self.speed_y
+
+        self.accelerate_ai_x()
+        self.accelerate_ai_y()
+
+        self.speed_x += float(self.a_x.real)
+        self.speed_y += float(self.a_y.real)
